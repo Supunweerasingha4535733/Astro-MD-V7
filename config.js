d5c5cb4441bc2d051ceaf25a247da3f520d87be9
Supunweerasingha4535733 <124757983+Supunweerasingha4535733@users.noreply.github.com>
2023-02-15 13:11:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ module.exports = {
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? 'default' : process.env.ALIVE_MSG,
 LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER === undefined ? 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
-INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
+FOOTER: process.env.FOOTER === undefined ? '꧁༺׀𝚂𝚄𝙿𝚄𝙽׀༻꧂' : process.env.FOOTER,
+CAPTION: process.env.CAPTION === undefined ? '*꧁༺׀𝚂𝚄𝙿𝚄𝙽׀༻꧂*' : process.env.CAPTION,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
+INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox වැඩ කරන්නෙ නෑ. මේ Number එක add කරල Group එකක් හදල ඒකෙන් වැඩ කරගන්න  ' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
 SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
 };
